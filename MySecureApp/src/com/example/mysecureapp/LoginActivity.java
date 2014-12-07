43d4e859d6bf754392fa2319f0fe24e9fcc8503f
Marvolo1991 <piotrek.pelka91@gmail.com>
2014-12-07 22:58:58 +0100

    Initial commit

---
+++ MySecureApp/src/com/example/mysecureapp/LoginActivity.java
@@ -0,0 +1,50 @@
+package com.example.mysecureapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LoginActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		
+		Button log_in_btn = (Button)findViewById(R.id.Login_login_btn);
+        log_in_btn.setOnClickListener(this);
+	}
+	
+	public void onClick(View v)
+    {
+    	if(v.getId() == R.id.Login_login_btn)
+    	{
+    		Intent Options_Intent = new Intent(this,Options_menu.class);
+    		startActivity(Options_Intent);
+    	}
+    }
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.login, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
---
+++ MySecureApp/src/com/example/mysecureapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mysecureapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity implements OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button log_in_btn = (Button)findViewById(R.id.LogIn_btn);
+        log_in_btn.setOnClickListener(this);
+    }
+    
+    public void onClick(View v)
+    {
+    	if(v.getId() == R.id.LogIn_btn)
+    	{
+    		Intent LogIn_Intent = new Intent(this,LoginActivity.class);
+    		startActivity(LogIn_Intent);
+    	}
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
